e27ea1fc3b1f8cf1b82d6444b7eab935456f6208
Nima77Moosavi <nima77moosavi@gmail.com>
2025-04-29 10:16:04 +0330

    1

---
+++ src/components/HeaderMobile/HeaderMobile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { IoMenu, IoSearch } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { GoHeartFill } from "react-icons/go";
+import { FaCartShopping } from "react-icons/fa6";
+
+const HeaderMobile = () => {
+  return (
+    <div>HeaderMobile</div>
+  )
+}
+
+export default HeaderMobile
