c9032f286b3e46a273bf8ccc25e199d01bf0b36e
Nima77Moosavi <nima77moosavi@gmail.com>
2025-05-06 09:06:31 +0330

    modified header for responsive view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header/Header";
-import HeaderMobile from "../components/HeaderMobile/HeaderMobile";
 import Highlights from "../components/Highlights/Highlights"; // Import the Highlights component
 import Collections from "../components/Collections/Collections";
-import styles from "./Home.module.css";
 import axios from "axios";
 import Bestsellers from "../components/Bestsellers/Bestsellers";
 import SpecialOffer from "../components/SpecialOffer/SpecialOffer";
 import SpecialProducts from "../components/SpecialProducts/SpecialProducts";
 import Footer from "../components/Footer/Footer";
 import HomePageHeader from "../components/HomePageHeader/HomePageHeader";
+import styles from "./Home.module.css";
 
 const Home = () => {
   const [latestProducts, setLatestProducts] = useState([]);
@@ -31,15 +30,16 @@ const Home = () => {
 
       <Header />
       <HomePageHeader />
-      <div className={styles.content}>
-        <Highlights />
-        <Collections />
+      
+        <div className={styles.content}>
+          <Highlights />
+          <Collections />
 
-        <Bestsellers />
-        <SpecialOffer />
-        <SpecialProducts />
-        {/* <ProductList products={latestProducts} /> */}
-      </div>
+          <Bestsellers />
+          <SpecialOffer />
+          <SpecialProducts />
+          {/* <ProductList products={latestProducts} /> */}
+        </div>
       <Footer />
     </div>
   );
