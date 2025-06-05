671514a2e3221cf7fb8fd71cdb56183842c61fd6
Nima77Moosavi <nima77moosavi@gmail.com>
2025-06-05 19:37:45 +0330

    modified banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,19 +1,15 @@
-import React, { useState, useEffect } from "react";
-import styles from "./Banner.module.css"
-import image1 from "../../assets/banner1.png";
-import image3 from "../../assets/img.png";
-
+import React from "react";
+import styles from "./Banner.module.css";
+import baseImage from "../../assets/img.png";  // This is your left-side image
+import textImage from "../../assets/banner1.png"; // This is your text overlay image
 
 const Banner = () => {
-
-  const [isMobile, setIsMobile] = useState(false);
-
   return (
     <div className={styles.container}>
-      <img src={image3} alt="" className={styles.dastImg}/>
-      <img src={image1} alt="" className={styles.textImg}/>
+      <img src={baseImage} alt="Background" className={styles.baseImg} />
+      <img src={textImage} alt="Banner Text" className={styles.textImg} />
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
