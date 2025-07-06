39f85aea0349dc0437e7edc2315a6e44f77c4308
fatemehghsmi <ghasemi12794@gmail.com>
2025-07-06 17:16:41 +0330

    keyfiat va logo

--- src/components/FeaturesLine/FeaturesLine.jsx
+++ src/components/FeaturesLine/FeaturesLine.jsx
@@ -10,7 +10,7 @@ const FeaturesLine = () => {
     { icon: <img src={installmentPng} alt="قسط" className={styles.pngIcon} />, text: "خرید اقساطی", id: 1 },
     { icon: <img src={truckPng} alt="ارسال" className={styles.pngIcon} />, text: "ارسال رایگان به سراسر کشور", id: 2 },
     { icon: <img src={returnPng} alt="مرجوعی" className={styles.pngIcon} />, text: "ضمانت مرجوعی", id: 3 },
-    { icon: <img src={shieldPng} alt="امنیت" className={styles.pngIcon} />, text: "ضمانت امنیت و اصالت", id: 4 },
+    { icon: <img src={shieldPng} alt="امنیت" className={styles.pngIcon} />, text: "ضمانت کیفیت و اصالت", id: 4 },
   ];
 
   return (
