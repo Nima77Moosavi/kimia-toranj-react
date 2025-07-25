c04e893a8d98df41d590ef0a139ba6cdb3369b4a
Nima77Moosavi <94492258+Nima77Moosavi@users.noreply.github.com>
2025-07-25 13:35:41 +0330

    sitemap and robots.txt

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePluginSitemap } from 'vite-plugin-sitemap';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginSitemap({
+      hostname: 'https://kimiatoranj.com', // <-- Change to your real domain!
+      routes: [
+        '/', '/login', '/blog', '/shop', '/gift-selector'
+        // Add dynamic routes below, e.g. /blog/1, /blog/2, etc.
+      ],
+    }),
+  ],
   build: {
     outDir: "build",
   },
