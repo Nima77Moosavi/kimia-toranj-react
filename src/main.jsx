b1d487dc31f1832c229f590ab5e42091cc0b2cd6
Nima77Moosavi <94492258+Nima77Moosavi@users.noreply.github.com>
2025-07-26 10:02:58 +0330

    skeleton

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './skeleton.css'
 import './fonts/Neirizi.ttf'
 import { FavoritesProvider } from './context/FavoritesContext';
 
